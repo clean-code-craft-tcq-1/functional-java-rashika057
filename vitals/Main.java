a59c5b4c19d95d4689dadc11f8391879e3393cfe
rashika057 <rashika059@gmail.com>
2021-03-12 09:16:29 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,52 +2,53 @@ package vitals;
 
 public class Main {
 		
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-    	boolean isTempOk = chargeTemparatureInRange(temperature);
-    	boolean issocOk = socInRange(soc);
-    	boolean ischargeRateOk = chargeRateInRange(chargeRate);
+    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {    	
+    	boolean isTempOk = false;
+    	boolean issocOk = false;
+    	boolean ischargeRateOk = false;
+			isTempOk = chargeTemparatureInRange(temperature);
+			issocOk = socInRange(soc);
+	    	ischargeRateOk = chargeRateInRange(chargeRate);    	
     	return (isTempOk && issocOk && ischargeRateOk);        
     }
     
-    static boolean chargeTemparatureInRange(float tempVal) {  
-    	Condition temperature = new Condition("Charge Temperature",0,45);
-    	return valueInRange(tempVal,temperature);
+    static boolean chargeTemparatureInRange(float tempVal) { 
+    	return printMessage("Charge Temperature",valueInRange(0, 45, tempVal));
     }
     
-    static boolean dischargeTemparatureInRange(float tempVal) {  
-    	Condition temperature = new Condition("Discharge Temperature",-20,60);
-    	return valueInRange(tempVal,temperature);
+    static boolean dischargeTemparatureInRange(float tempVal)  {  
+    	return printMessage("Discharge Temperature",valueInRange(-20, 60, tempVal));
     }
     
     static boolean socInRange(float socVal) {
-    	Condition soc = new Condition("State of Charge",20,80);
-    	return valueInRange(socVal,soc);
+    	return printMessage("State of Charge",valueInRange(20, 80, socVal));
     }
     
     static boolean chargeRateInRange(float chargeRateVal) {
-    	Condition chargeRate = new Condition("Charge Rate",0,0.8f);
-    	return valueInRange(chargeRateVal,chargeRate);
+    	return printMessage("Charge Rate",valueInRange(0, 0.8f, chargeRateVal));
     }
        
-    static boolean valueInRange(float value,Condition cond) {
-    	if(value < cond.getMin()) {
-    		System.out.println(cond.getName()+" is low!");
-            return false;
+    static String valueInRange(float min,float max,float value) {
+    	if(value < min) {
+            return " is low!";
         }
-    	else if(value > cond.getMax()) {
-    		System.out.println(cond.getName()+" is high!");
-            return false;
+    	else if(value > max) {
+            return " is high";
         }
+    	return "";
+    } 
+    
+    public static boolean printMessage(String name, String msg) {
+    	if(!msg.isEmpty()) {   	
+    	System.out.println(name+msg);
+    	return false;
+    	}
     	return true;
-    }    
+    }
 
-    public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        assert(batteryIsOk(40, 60, 0.9f) == false);
-        assert(chargeTemparatureInRange(60) == false);
-        assert(dischargeTemparatureInRange(60) == true);
-        assert(socInRange(10) == false);
-        assert(chargeRateInRange(1.2f) == false);
+    public static void main(String[] args) {      
+			assert(batteryIsOk(25, 70, 0.7f) == true);
+			assert(batteryIsOk(50, 85, 0.0f) == false);
+	        assert(batteryIsOk(40, 60, 0.9f) == false);  		     
     }
 }
